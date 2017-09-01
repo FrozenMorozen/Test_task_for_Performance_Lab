a800ed7294cdca80a4e72c9a9699e65ff8c4b4da
Morozov_Denis <morozovd96@outlook.com>
2017-09-02 05:07:36 +0700

    Update test case #1

--- TestTaskMorozov/src/com/performancelab/calc/Main.java
+++ TestTaskMorozov/src/com/performancelab/calc/Main.java
@@ -3,8 +3,8 @@ package com.performancelab.calc;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        CalcForDividing calcForDividing=new CalcForDividing();
+        // write your code here
+        CalcForDividing calcForDividing = new CalcForDividing();
         calcForDividing.launchApp();
     }
 }
