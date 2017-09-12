91a49c169d7526d2c9177c8047d0576177f4c15e
Morozov_Denis <morozovd96@outlook.com>
2017-09-12 12:55:46 +0700

    Add auto test testDivideHardNumber

--- TestTaskMorozov/src/Testing/CalcForDividingTest.java
+++ TestTaskMorozov/src/Testing/CalcForDividingTest.java
@@ -12,24 +12,6 @@ public class CalcForDividingTest {
     private static final double DELTA = 1e-15;
 
     @Test
-    public void testActionPerformed(){
-        double minForRnd=0.265;
-        double maxForRnd=1000.6632;
-        double[] number=new double[10];
-
-        CalcForDividing calcForDividing=new CalcForDividing();
-        JTextField textField=new JTextField();
-
-        for (int i=0;i<number.length;i++){
-            number[i]=rnd(minForRnd, maxForRnd);
-            textField.setText(String.valueOf(number[i]));
-            boolean testCurrentValue=calcForDividing.checkCurrentValue(textField);
-            assertTrue("Значение верно",testCurrentValue);
-            //assertFalse("Значение неверно",testCurrentValue);
-        }
-    }
-
-    @Test
     public void testDivideWholeNumbers() throws Exception {
         double firstArg=54;
         double secondArg=6;
@@ -45,10 +27,12 @@ public class CalcForDividingTest {
         assertEquals(firstArg/secondArg,calcForDividing.divide(firstArg,secondArg),DELTA);
     }
 
-    private static double rnd(double min, double max)
-    {
-        max -= min;
-        return (int) (Math.random() * ++max) + min;
+    @Test
+    public void testDivideHardNumber() throws Exception {
+        double firstArg=5.456168318;
+        double secondArg=12.416816;
+        CalcForDividing calcForDividing=new CalcForDividing();
+        assertEquals(firstArg/secondArg,calcForDividing.divide(firstArg,secondArg),DELTA);
     }
 
 
