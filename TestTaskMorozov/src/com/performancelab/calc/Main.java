6044fc58a794febc9b073b9398a202f0fb3d120f
DENIS <morozovd96@outlook.com>
2018-02-05 23:31:24 +0700

    Experiment on ssh cloning

--- TestTaskMorozov/src/com/performancelab/calc/Main.java
+++ TestTaskMorozov/src/com/performancelab/calc/Main.java
@@ -3,6 +3,7 @@ package com.performancelab.calc;
 public class Main {
 
     public static void main(String[] args) {
+        //dkdkdkd
 
         CalcForDividing calcForDividing = new CalcForDividing();
         calcForDividing.launchApp();
